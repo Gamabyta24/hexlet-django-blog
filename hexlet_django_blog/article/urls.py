fd78d4d7e420c07beced8149b5efb805d7e31dfb
Gamabyta24 <Gamabyta@gmail.com>
2025-03-03 11:04:46 +0300

    add migrations

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.HomePageView.as_view(),name='home'),
+    path('<str:tags>/<int:article_id>/', views.IndexArticle.as_view(),name='article'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.urls import reverse
 
 def index(request):
     context={'app_name':'hexlet_django_blog'}
     return render(request, 'article/index.html',context=context)
+
+class IndexArticle(View):
+    def get(self,request,tags,article_id,*args,**kwargs):
+        context={'article_id':article_id,'tags':tags}
+        return render(request, 'article/index.html',context=context)
+    
+class HomePageView(View):
+    def get(self,request,*args,**kwargs):
+        url = reverse('article',kwargs={'tags': 'python', 'article_id': 42})
+        return redirect(url)
